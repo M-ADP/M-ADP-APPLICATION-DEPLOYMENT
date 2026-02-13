81fe9af1134dc10b67deb921365da1fc93161639
Lian08 <lian080408@gmail.com>
2026-02-13 17:20:56 +0900

    Initial Commit

---
+++ src/main/java/madp/appdeployment/MadpAppdeploymentApplication.java
@@ -0,0 +1,13 @@
+package madp.appdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadpAppdeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadpAppdeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/madp/appdeployment/MadpAppdeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package madp.appdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadpAppdeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
