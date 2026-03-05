a49ba666b6648f438ef522f3b0a4e0a55088ffa1
jaeminjo732 <jaeminjo732@gmail.com>
2026-03-05 19:20:20 +0900

    fix(appdeployment): ProjectClientFallback 시그니처 동기화

--- src/main/java/madp/appdeployment/domain/infrastructure/client/fallback/ProjectClientFallback.java
+++ src/main/java/madp/appdeployment/domain/infrastructure/client/fallback/ProjectClientFallback.java
@@ -4,18 +4,19 @@ import madp.appdeployment.domain.exception.ProjectServiceUnavailableException;
 import madp.appdeployment.domain.infrastructure.client.ProjectClient;
 import madp.appdeployment.domain.infrastructure.client.response.ProjectAvailableResponseDto;
 import madp.appdeployment.domain.infrastructure.client.response.ProjectOwnerResponseDto;
+import madp.appdeployment.global.presentation.response.dto.ApiResponseDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProjectClientFallback implements ProjectClient {
 
     @Override
-    public ProjectAvailableResponseDto getProjectAvailable(String projectId) {
+    public ApiResponseDto<ProjectAvailableResponseDto> getProjectAvailable(String projectId) {
         throw new ProjectServiceUnavailableException();
     }
 
     @Override
-    public ProjectOwnerResponseDto getProjectOwner(String projectId) {
+    public ApiResponseDto<ProjectOwnerResponseDto> getProjectOwner(String projectId) {
         throw new ProjectServiceUnavailableException();
     }
 }
