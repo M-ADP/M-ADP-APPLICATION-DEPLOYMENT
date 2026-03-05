0e8a1d220d011d34c7b72447665e2a7dcce9ef62
jaeminjo732 <jaeminjo732@gmail.com>
2026-03-06 00:35:52 +0900

    feat(appdeployment): AppRevisionResponseDto 추가

---
+++ src/main/java/madp/appdeployment/domain/infrastructure/client/response/AppRevisionResponseDto.java
@@ -0,0 +1,21 @@
+package madp.appdeployment.domain.infrastructure.client.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record AppRevisionResponseDto(
+        @JsonProperty("application_id")
+        String applicationId,
+
+        @JsonProperty("max_cpu")
+        Integer maxCpu,
+
+        @JsonProperty("max_memory")
+        Integer maxMemory,
+
+        @JsonProperty("max_disk")
+        Integer maxDisk,
+
+        @JsonProperty("revised")
+        boolean revised
+) {
+}
