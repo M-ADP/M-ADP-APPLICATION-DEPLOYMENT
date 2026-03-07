c9ceb83f4b966ca27e9b4128ec18d945f4f465b9
Lian08 <lian080408@gmail.com>
2026-03-07 17:17:40 +0900

    feat(app-deployment) :: snake_case로 변경

--- src/main/java/madp/appdeployment/domain/infrastructure/client/request/JenkinsDeploymentRequestDto.java
+++ src/main/java/madp/appdeployment/domain/infrastructure/client/request/JenkinsDeploymentRequestDto.java
@@ -1,16 +1,24 @@
 package madp.appdeployment.domain.infrastructure.client.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.NonNull;
 
 @Builder
 public record JenkinsDeploymentRequestDto(
     @NonNull
+    @JsonProperty("repository_full_name")
     String repositoryFullName,
+
     @NonNull
+    @JsonProperty("branch")
     String branch,
+
     @NonNull
+    @JsonProperty("image_name")
     String imageName,
+
     @NonNull
+    @JsonProperty("repository_id")
     Long repositoryId
 ) {}
