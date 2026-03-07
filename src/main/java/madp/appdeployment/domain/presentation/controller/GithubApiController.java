5d1dc411780fb13e230a400d70445eb0cd017a5c
Lian08 <lian080408@gmail.com>
2026-03-07 18:52:46 +0900

    feat(app-deployment) :: /apps 붙이기

--- src/main/java/madp/appdeployment/domain/presentation/controller/GithubApiController.java
+++ src/main/java/madp/appdeployment/domain/presentation/controller/GithubApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/github")
+@RequestMapping("/apps/github")
 @RequiredArgsConstructor
 public class GithubApiController {
     private final GithubWebhookAsyncService githubWebhookAsyncService;
--- src/main/java/madp/appdeployment/domain/presentation/controller/JenkinsController.java
+++ src/main/java/madp/appdeployment/domain/presentation/controller/JenkinsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/jenkins")
+@RestController("/apps/jenkins")
 @RequiredArgsConstructor
 public class JenkinsController {
     private final JenkinsService jenkinsService;
