3081b499839f4e8447a3a322a23da170a7ae2fd9
Lian08 <lian080408@gmail.com>
2026-03-10 08:05:11 +0900

    feat(app-deployment) :: JenkinsCallBackRequestDto updated

--- src/main/java/madp/appdeployment/domain/presentation/dto/request/JenkinsCallBackRequestDto.java
+++ src/main/java/madp/appdeployment/domain/presentation/dto/request/JenkinsCallBackRequestDto.java
@@ -1,8 +1,10 @@
 package madp.appdeployment.domain.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
 public record JenkinsCallBackRequestDto(
+        @JsonProperty("repository_id")
         @NotNull(message = "Repository 아이디는 null일 수 없습니다.")
         Long repositoryId
 ) {}
