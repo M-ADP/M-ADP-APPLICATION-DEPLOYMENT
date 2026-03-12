b559d725241535612b4c7d33daf1c72b9cbcc302
jaeminjo732 <jaeminjo732@gmail.com>
2026-03-12 19:32:48 +0900

    refactor(app-deployment) :: AppRevisionResponseDto max_memory 타입을 Double로 변경

--- src/main/java/madp/appdeployment/domain/infrastructure/client/response/AppRevisionResponseDto.java
+++ src/main/java/madp/appdeployment/domain/infrastructure/client/response/AppRevisionResponseDto.java
@@ -10,7 +10,7 @@ public record AppRevisionResponseDto(
         Integer maxCpu,
 
         @JsonProperty("max_memory")
-        Integer maxMemory,
+        Double maxMemory,
 
         @JsonProperty("max_disk")
         Integer maxDisk,
