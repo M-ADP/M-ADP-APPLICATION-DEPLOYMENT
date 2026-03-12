5af0d39eb1bf32b55ed09906e54c8512892dacfb
jaeminjo732 <jaeminjo732@gmail.com>
2026-03-12 19:32:47 +0900

    feat(app-deployment) :: 생성 요청 메모리 제약을 0.25~1GB로 변경

--- src/main/java/madp/appdeployment/domain/presentation/dto/request/CreateAppDeploymentRequestDto.java
+++ src/main/java/madp/appdeployment/domain/presentation/dto/request/CreateAppDeploymentRequestDto.java
@@ -24,14 +24,14 @@ public record CreateAppDeploymentRequestDto(
 
         @JsonProperty("memory")
         @NotNull(message = "메모리는 존재해야합니다.")
-        @Min(value = 512, message = "메모리는 최소 512MB 이상이어야 합니다.")
-        @Max(value = 2048, message = "메모리는 최대 2048MB 이하이어야 합니다.")
-        Integer memory,
+        @DecimalMin(value = "0.25", message = "메모리는 최소 0.25GB 이상이어야 합니다.")
+        @DecimalMax(value = "1.0", message = "메모리는 최대 1GB 이하이어야 합니다.")
+        Double memory,
 
         @JsonProperty("disk")
         @NotNull(message = "디스크 크기는 존재해야합니다.")
         @Min(value = 2, message = "디스크 크기는 최소 2GB 이상이어야 합니다.")
-        @Max(value = 16, message = "디스크 크기는 최대 16GB 이하이어야 합니다.")
+        @Max(value = 50, message = "디스크 크기는 최대 50GB 이하이어야 합니다.")
         Integer disk,
 
         @JsonProperty("project_id")
