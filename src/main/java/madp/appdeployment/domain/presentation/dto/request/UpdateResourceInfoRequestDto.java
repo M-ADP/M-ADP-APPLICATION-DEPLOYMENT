3ee2c84f6452add393c8b36f32bf655a1271026d
jaeminjo732 <jaeminjo732@gmail.com>
2026-03-12 19:32:48 +0900

    feat(app-deployment) :: 리소스 수정 요청 메모리 제약을 0.25~1GB로 변경

--- src/main/java/madp/appdeployment/domain/presentation/dto/request/UpdateResourceInfoRequestDto.java
+++ src/main/java/madp/appdeployment/domain/presentation/dto/request/UpdateResourceInfoRequestDto.java
@@ -1,6 +1,8 @@
 package madp.appdeployment.domain.presentation.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -18,13 +20,13 @@ public record UpdateResourceInfoRequestDto(
 
         @JsonProperty("max_memory")
         @NotNull(message = "메모리는 존재해야합니다.")
-        @Min(value = 512, message = "메모리는 최소 512MB 이상이어야 합니다.")
-        @Max(value = 2048, message = "메모리는 최대 2048MB 이하이어야 합니다.")
-        Integer memory,
+        @DecimalMin(value = "0.25", message = "메모리는 최소 0.25GB 이상이어야 합니다.")
+        @DecimalMax(value = "1.0", message = "메모리는 최대 1GB 이하이어야 합니다.")
+        Double memory,
 
         @JsonProperty("max_disk")
         @NotNull(message = "디스크 크기는 존재해야합니다.")
         @Min(value = 2, message = "디스크 크기는 최소 2GB 이상이어야 합니다.")
-        @Max(value = 16, message = "디스크 크기는 최대 16GB 이하이어야 합니다.")
+        @Max(value = 50, message = "디스크 크기는 최대 50GB 이하이어야 합니다.")
         Integer disk
 ) {}
