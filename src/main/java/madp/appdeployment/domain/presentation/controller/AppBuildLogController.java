b90e0a69284af46d50046217ca933d4b5e7f8772
박동현 <pdh0128a@gmail.com>
2026-04-17 14:43:59 +0900

    fix(app-deployment): API Response 형식 수정

--- src/main/java/madp/appdeployment/domain/presentation/controller/AppBuildLogController.java
+++ src/main/java/madp/appdeployment/domain/presentation/controller/AppBuildLogController.java
@@ -5,7 +5,7 @@ import madp.appdeployment.domain.application.service.AppBuildLogService;
 import madp.appdeployment.domain.presentation.dto.response.AppBuildLogDetailResponseDto;
 import madp.appdeployment.domain.presentation.dto.response.AppBuildLogListResponseDto;
 import madp.appdeployment.domain.presentation.dto.response.AppLatestBuildLogResponseDto;
-import madp.appdeployment.global.presentation.dto.response.LegacyApiResponseDto;
+import madp.appdeployment.global.presentation.dto.response.ApiResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,12 +20,12 @@ public class AppBuildLogController {
     private final AppBuildLogService appBuildLogService;
 
     @GetMapping("/{project_id}/{name}/build-logs")
-    public ResponseEntity<LegacyApiResponseDto<AppBuildLogListResponseDto>> getBuildLogs(
+    public ResponseEntity<ApiResponseDto<AppBuildLogListResponseDto>> getBuildLogs(
             @PathVariable("project_id") String projectId,
             @PathVariable("name") String name
     ) {
         return ResponseEntity.ok(
-                LegacyApiResponseDto.of(
+                ApiResponseDto.of(
                         "App deployment build logs retrieved successfully",
                         appBuildLogService.getBuildLogs(projectId, name)
                 )
@@ -33,12 +33,12 @@ public class AppBuildLogController {
     }
 
     @GetMapping("/{project_id}/{name}/build-logs/latest")
-    public ResponseEntity<LegacyApiResponseDto<AppLatestBuildLogResponseDto>> getLatestBuildLog(
+    public ResponseEntity<ApiResponseDto<AppLatestBuildLogResponseDto>> getLatestBuildLog(
             @PathVariable("project_id") String projectId,
             @PathVariable("name") String name
     ) {
         return ResponseEntity.ok(
-                LegacyApiResponseDto.of(
+                ApiResponseDto.of(
                         "App deployment latest build log retrieved successfully",
                         appBuildLogService.getLatestBuildLog(projectId, name)
                 )
@@ -46,13 +46,13 @@ public class AppBuildLogController {
     }
 
     @GetMapping("/{project_id}/{name}/build-logs/{build_number}")
-    public ResponseEntity<LegacyApiResponseDto<AppBuildLogDetailResponseDto>> getBuildLogDetail(
+    public ResponseEntity<ApiResponseDto<AppBuildLogDetailResponseDto>> getBuildLogDetail(
             @PathVariable("project_id") String projectId,
             @PathVariable("name") String name,
             @PathVariable("build_number") Integer buildNumber
     ) {
         return ResponseEntity.ok(
-                LegacyApiResponseDto.of(
+                ApiResponseDto.of(
                         "App deployment build log detail retrieved successfully",
                         appBuildLogService.getBuildLogDetail(projectId, name, buildNumber)
                 )
